b8b8c6262cb04c2e1668397bee8b7a91a01c086a
GitHub Action <action@github.com>
2024-04-26 19:14:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/dittonjs/examplejetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dittonjs/examplejetbrainsplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dittonjs.examplejetbrainsplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dittonjs.examplejetbrainsplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
